f6dc1f19ec16ec15b188f6159c1606ccb9d4068c
Meako <meako689@gmail.com>
2009-05-14 16:10:54 +0300

    Minor changes to tests

--- pumpkin/tests/test.py
+++ pumpkin/tests/test.py
@@ -2,17 +2,20 @@
 """tests for pumpkin app"""
 from pumpkin import parser
     
-def test_empty_text():
+def test_empty():
+    """parse with empty text as parameter"""
     text = ""
     feature = parser.parse(text)
     assert feature.description == None
 
 def test_some_text():
+    """parse with some text, Not the feature definition"""
     text = "blablabla"
     feature = parser.parse(text)
-    assert feature.description != text
+    assert feature.description == None
 
-def test_right_text():
-    text = "Feature: Testing"
+def test_right():
+    """parse valid feature definition"""
+    text = """Feature: Testing"""
     feature = parser.parse(text)
     assert feature.description == text
